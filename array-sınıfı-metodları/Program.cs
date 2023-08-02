e887d9e40897edc7d3535ff89932e1631a544fbf
Furkan Uçar <furkanucar229@gmail.com>
2023-08-02 20:09:20 +0300

    first commit

--- array-sınıfı-metodları/Program.cs
+++ array-sınıfı-metodları/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] sayıDizisi = {1,15,6,27,38,9,};
+﻿int[] sayıDizisi = { 1, 15, 6, 27, 38, 9, };
 
 foreach (var sayi in sayıDizisi)
 {
@@ -23,17 +23,17 @@ foreach (var sayi in sayıDizisi)
 
 
 Console.WriteLine("---------Temizleme------------");
-Array.Clear(sayıDizisi,2,2);
-foreach(var sayi in sayıDizisi)
+Array.Clear(sayıDizisi, 2, 2);
+foreach (var sayi in sayıDizisi)
 {
     Console.WriteLine(sayi);
 }
 
 Console.WriteLine("---------array indexof------------");
-Console.WriteLine(Array.IndexOf(sayıDizisi,27));
+Console.WriteLine(Array.IndexOf(sayıDizisi, 27));
 
 Console.WriteLine("---------dizi ekleme-----");
-Array.Resize<int>(ref sayıDizisi,6);
+Array.Resize<int>(ref sayıDizisi, 6);
 sayıDizisi[5] = 99;
 
 foreach (var sayi in sayıDizisi)
